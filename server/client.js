23a4dda120afbbbe0beb9bf24e2ef6027c0ca2e6
Harshil Patel <omjoshi2217@gmail.com>
2026-01-08 02:37:14 +0530

    perf: optimize puppeteer args for faster startup

--- server/client.js
+++ server/client.js
@@ -11,7 +11,17 @@ export const initClient = () => {
     client = new Client({
         authStrategy: new LocalAuth(),
         puppeteer: {
-            args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            headless: true,
+            args: [
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--disable-dev-shm-usage',
+                '--disable-accelerated-2d-canvas',
+                '--no-first-run',
+                '--no-zygote',
+                '--single-process', // <- this one doesn't works in Windows
+                '--disable-gpu'
+            ],
         }
     });
 
